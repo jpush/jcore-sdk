abbbf856c38f4b9c9a3bfdff7c2262fa77e532b8
huangshuni <15889638723@163.com>
2025-11-03 10:40:04 +0800

    update 5.3.0-jbasic and support spm

---
+++ Package.swift
@@ -0,0 +1,44 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "JCore",
+    platforms: [
+        .iOS(.v8)
+    ],
+    products: [
+        .library(
+            name: "JCore",
+            targets: ["JCoreWrapper"]
+        ),
+    ],
+    targets: [
+        .target(
+            name: "JCoreWrapper",
+            dependencies: ["JCore"],
+            path: "JCoreWrapper",
+            linkerSettings: [
+                .linkedLibrary("z"),
+                .linkedLibrary("resolv"),
+                .linkedFramework("UIKit"),
+                .linkedFramework("CFNetwork"),
+                .linkedFramework("CoreFoundation"),
+                .linkedFramework("CoreTelephony"),
+                .linkedFramework("SystemConfiguration"),
+                .linkedFramework("CoreGraphics"),
+                .linkedFramework("Foundation"),
+                .linkedFramework("WebKit"),
+                .linkedFramework("Security"),
+                .linkedFramework("UserNotifications", .when(platforms: [.iOS])),
+                .linkedFramework("Network", .when(platforms: [.iOS])),
+            ]
+        ),
+        .binaryTarget(
+            name: "JCore",
+            path: "jbasic-ios-5.3.0.xcframework"
+        ),
+    ]
+)
+
