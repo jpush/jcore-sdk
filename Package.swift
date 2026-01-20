669c64c6f69c5e06cf836e367617c10da9976c15
huangshuni <15889638723@163.com>
2026-01-20 11:42:33 +0800

    update 5.3.0-noidfa and support spm

--- Package.swift
+++ Package.swift
@@ -37,7 +37,7 @@ let package = Package(
         ),
         .binaryTarget(
             name: "JCore",
-            path: "jbasic-ios-5.3.0.xcframework"
+            path: "jcore-noidfa-ios-5.3.0.xcframework"
         ),
     ]
 )
