7cabadc780a1f6504d47805bfbf9952b43ceea90
huangshuni <15889638723@163.com>
2026-04-27 11:58:18 +0800

    update 5.4.0-noidfa

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "JCore",
-            path: "jbasic-ios-5.4.0.xcframework"
+            path: "jcore-noidfa-ios-5.4.0.xcframework"
         ),
     ]
 )
