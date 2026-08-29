e7679dd7ed102fc4506b8c54b78a5bb513aa6f4c
rechar <lilx@jiguang.cn>
2026-08-29 11:35:22 +0800

    发布：更新 JCore iOS SDK 至 4.7.3-noidfa

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "JCore",
+    platforms: [
+        .iOS(.v8)
+    ],
+    products: [
+        .library(
+            name: "JCore",
+            targets: ["JCore"]
+        ),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "JCore",
+            path: "jcore-noidfa-ios-4.7.3.xcframework"
+        ),
+    ]
+)
