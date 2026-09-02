35f531ab0349af1dcd5742c2f102b35128d97e5f
rechar <lilx@jiguang.cn>
2026-09-02 18:52:13 +0800

    发布：更新 JCore iOS SDK 至 5.5.2-jbasic

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "JCore",
-            path: "jcore-noidfa-ios-5.5.2.xcframework"
+            path: "jbasic-ios-5.5.2.xcframework"
         ),
     ]
 )
